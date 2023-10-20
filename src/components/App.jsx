620a5b83403420902241182f6197a96c4ad8a077
Olena <shapovalev@gmail.com>
2023-10-20 15:43:15 +0200

    contacts

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,29 +4,29 @@ import HomePage from 'pages/HomePage';
 import RegisterPage from 'pages/RegisterPage';
 import LoginPage from 'pages/LoginPage';
 import Layout from 'components/Layout';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { useEffect } from 'react';
 import { refreshUser } from 'redux/auth/operations';
 
 const App = () => {
-const dispatch = useDispatch();
-
-useEffect(() => {
-  dispatch(refreshUser())
-}, [dispatch]);
+  const dispatch = useDispatch();
+  
+  useEffect(() => {
+    dispatch(refreshUser());
+  }, [dispatch]);
 
   return (
     <div>
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<HomePage />} />
-          <Route path="contacts" element={<ContactsPage />} />
+         <Route path="contacts" element={<ContactsPage />} /> 
           <Route path="register" element={<RegisterPage />} />
           <Route path="login" element={<LoginPage />} />
         </Route>
       </Routes>
     </div>
-  );
+  ); 
 };
 
 export default App;
